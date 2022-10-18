afcbf0da1a36a469e6b251d2c84738c02fc63f31
Kostiantyn Kalchenko <scootertrance7@gmail.com>
2022-10-18 19:40:27 +0300

    added catching exceptions

--- Lesson1/numbers-operations/src/operations/Multiplication.java
+++ Lesson1/numbers-operations/src/operations/Multiplication.java
@@ -1,6 +1,6 @@
 package operations;
 
-class Multiply {
+class Multiplication {
     public static void getMultiPlay(MyNumbers myNumbers) {
         int multiPly = myNumbers.getNumber1() * myNumbers.getNumber2();
         System.out.println(multiPly);
--- Lesson1/numbers-operations/src/operations/Operations.java
+++ Lesson1/numbers-operations/src/operations/Operations.java
@@ -2,18 +2,29 @@ package operations;
 
 public class Operations {
     public static void main(String[] args) {
-        MyNumbers myNumb = new MyNumbers(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-        String operation = args[2]; // An executable operation;
-        switch (operation) {
-            case "sum":
-                Sum.getSum(myNumb);
-                break;
-            case "multiplay" :
-                Multiply.getMultiPlay(myNumb);
-            case "subtraction" :
-                Subtraction.getSubtraction(myNumb);
-            case "division" :
-                Division.getDivision(myNumb);
+        try {
+            MyNumbers myNumb = new MyNumbers(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+            String operation = args[2]; // An executable operation;
+
+            switch (operation) {
+                case "sum":
+                    Sum.getSum(myNumb);
+                    break;
+                case "multiplication":
+                    Multiplication.getMultiPlay(myNumb);
+                    break;
+                case "subtraction":
+                    Subtraction.getSubtraction(myNumb);
+                    break;
+                case "division":
+                    Division.getDivision(myNumb);
+                    break;
+                default:
+                    System.out.println("Available operations: sum, multiplication, subtraction, division.");
+            }
+
+        } catch (NumberFormatException e) {
+            System.out.println("Wrong data type. Enter a two numbers.");
         }
     }
 }
